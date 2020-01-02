2721e0ecbb7c25ad3a7e7d299906377b06bcd485
AZHAR JUMAN P <58019252+Azharjuman96@users.noreply.github.com>
2020-01-02 11:42:03 +0530

    Add files via upload

---
+++ consoleclass/ExceptionHandlingExample1.java
@@ -0,0 +1,23 @@
+package consoleclass;
+
+public class ExceptionHandlingExample1 {
+	static void check() throws ArithmeticException
+	{
+		System.out.println("Inside check function");
+		throw new ArithmeticException("Face Kollam");
+		
+	}
+	public static void main(String[]args)
+	{
+		try
+		{
+			check();
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("caught "+e);
+		}
+	}
+	
+
+}
