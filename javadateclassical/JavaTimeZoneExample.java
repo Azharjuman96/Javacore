f6db27c99f9c1b74a12d9e98a3aa1302cff45465
AZHAR JUMAN P <58019252+Azharjuman96@users.noreply.github.com>
2020-01-07 15:50:22 +0530

    Add files via upload

---
+++ javadateclassical/JavaTimeZoneExample.java
@@ -0,0 +1,23 @@
+package javadateclassical;
+
+import java.util.*;
+
+public class JavaTimeZoneExample 
+{
+	public static void main(String[] args)
+	{
+		String[] id=TimeZone.getAvailableIDs();
+		System.out.println("In Timezone class available Ids are: ");
+		for(int i=0;i<id.length;i++)
+		{
+			System.out.println(id[i]);
+		}
+		TimeZone zone=TimeZone.getTimeZone("Asia/Kolkata");
+		System.out.println("The Offset value of TimeZone: "+zone.getOffset(Calendar.ZONE_OFFSET));
+		System.out.println("Value of ID is: "+zone.getID());
+		TimeZone zone1=TimeZone.getDefault();
+		String name=zone1.getDisplayName();
+		System.out.println("Display name for default time zone: "+name);
+	}
+
+}
